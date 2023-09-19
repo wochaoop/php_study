3774d640a6828c0e545a28e798b0be0232487185
xiwangly2 <1334850101@qq.com>
2023-09-19 16:38:54 +0800

    面向对象测试

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo("Hello World");
+class Person {
+    public $name, $sex;
+ 
+    function __construct($name,$sex){
+        $this->name = $name;
+        $this->sex = $sex;
+    }
+
+    function getName(){
+        return $this->name;
+    }
+
+}
+ 
+$newObj = new Person("Tim","girl");
+
+echo $newObj->getName();
+
 ?>
